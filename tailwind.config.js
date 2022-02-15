192b153472b0cbe2ac0183a7457b1b9bdaf1b0bd
Hudson <thewhittakers6@gmail.com>
2022-02-15 06:42:19 -0600

    added poppins; container max width

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,html}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
