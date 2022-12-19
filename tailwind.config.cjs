1e2b921b4dd4d6e1a3114cfc8f00652916293a91
Hudson <thewhittakers6@gmail.com>
2022-12-19 06:20:42 -0600

    favicon, buncha pages, new index page, 404

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,html}"],
+  content: ["./index.astro", "./src/**/*.{js,astro}"],
   darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
       },
+      colors: {
+        black: {
+          50: '#808080',
+          100: '#737373',
+          200: '#666666',
+          300: '#5a5a5a',
+          400: '#4d4d4d',
+          500: '#404040',
+          600: '#333333',
+          700: '#262626',
+          800: '#1a1a1a',
+          900: '#0d0d0d',
+          DEFAULT: '#000000',
+        },
+      },
     },
-  },
-  plugins: [require("tailwindcss-animation-delay")],
+    plugins: [require("tailwindcss-animation-delay")],
+  }
 };
