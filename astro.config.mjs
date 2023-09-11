65c5a200a063d9fc52b4a245087db730ef699756
Hudson Whittaker <hudson@mirusresearch.com>
2023-09-11 10:47:41 -0500

    write webdev content; add noise

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
+// https://astro.build/config
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-import alpinejs from "@astrojs/alpinejs";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(), tailwind()],
+  integrations: [tailwind()],
 });
