87a32c467507ed6af0ce741b2572428a8d3a5e6b
Hudson Whittaker <hudsonwhittaker99@gmail.com>
2023-11-04 04:30:49 -0500

    page transitions, fix broken images, start adding vanta

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // https://astro.build/config
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    integrations: [tailwind(), alpinejs()],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,28 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.astro", "./src/**/*.{js,astro}"],
-  darkMode: "media",
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Poppins", "sans-serif"],
-      },
-      colors: {
-        black: {
-          50: "#808080",
-          100: "#737373",
-          200: "#666666",
-          300: "#5a5a5a",
-          400: "#4d4d4d",
-          500: "#404040",
-          600: "#333333",
-          700: "#262626",
-          800: "#1a1a1a",
-          900: "#0d0d0d",
-          DEFAULT: "#000000",
+    content: ['./index.astro', './src/**/*.{js,astro}'],
+    darkMode: 'media',
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Poppins', 'sans-serif'],
+            },
+            colors: {
+                black: {
+                    50: '#808080',
+                    100: '#737373',
+                    200: '#666666',
+                    300: '#5a5a5a',
+                    400: '#4d4d4d',
+                    500: '#404040',
+                    600: '#333333',
+                    700: '#262626',
+                    800: '#1a1a1a',
+                    900: '#0d0d0d',
+                    DEFAULT: '#000000',
+                },
+            },
         },
-      },
+        plugins: [
+            require('tailwindcss-animation-delay'),
+            'prettier-plugin-tailwindcss',
+        ],
     },
-    plugins: [require("tailwindcss-animation-delay")],
-  },
 };
