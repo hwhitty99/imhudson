4007ee7d71ffc46c27aa8b872eb517823fa9730b
Hudson Whittaker <hudsonwhittaker99@gmail.com>
2023-11-04 06:03:26 -0500

    switch icon handler, switch main font, overkill nav

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Poppins', 'sans-serif'],
+                sans: ['Azeret Mono Variable', 'mono'],
             },
             colors: {
                 black: {
