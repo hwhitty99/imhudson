3eb84e38882cb7a35ed44b498b7daf4adc89635c
Hudson Whittaker <hudsonwhittaker99@gmail.com>
2024-05-20 23:55:45 -0500

    update npm packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 // https://astro.build/config
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import alpinejs from '@astrojs/alpinejs';
+
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), alpinejs()],
+    integrations: [
+        tailwind(),
+        alpinejs(),
+        icon({
+            include: {
+                ic: ['round-arrow-left', 'round-arrow-right'],
+                octicon: ['triangle-up-24'],
+            },
+        }),
+    ],
+    image: {
+        domains: ['res.cloudinary'],
+    },
 });
